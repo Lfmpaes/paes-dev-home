4d560f5f5609a8d052a23b95c296d637dd07be4a
Luiz Fernando Paes <luiz@lfmpaes.com.br>
2023-06-14 00:17:20 -0300

    Looks like I started to think and added the pic to the project

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,7 @@ export default function Header() {
         <div className="header bg-green-7000 container items-center flex flex-col mx-auto p-4 lg:pt-16 pt-6">
             <Image
                 className="rounded-full"
-                src={
-                    'https://scontent.fcgh14-1.fna.fbcdn.net/v/t39.30808-6/354052608_23871600895764131_1431833224345519933_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=gjBgJOVXSesAX_0ytYf&_nc_ht=scontent.fcgh14-1.fna&oh=00_AfB31WYJPuPOYw3k0riKW-ksFnadoFZ_RUbN0FinMlWbdg&oe=648ED08F'
-                }
+                src={'/profilepic.jpg'}
                 width={128}
                 height={128}
                 alt={'Luiz Fernando M. Paes'}
