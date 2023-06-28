b2b9db5979cde53b90d7d0ef09d1413e0753cbc3
Luiz Fernando Paes <luiz@lfmpaes.com.br>
2023-06-28 12:35:00 -0300

    Installed TailwindCSS and started development

---
+++ app/components/ProfileCard.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Text } from '@nextui-org/react';
+
+export default function ProfileCard() {
+    return (
+        <div className="text-lfm-white">
+            <h3>Luiz Fernando M. Paes</h3>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+// /* Black: #1b222a */
+// /* White: #f2f4f1 */
+// /* Red: #d90231 */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            'lfm-black': '#1b222a',
+            'lfm-white': '#f2f4f1',
+            'lfm-red': '#d90231',
+        },
+        plugins: [require('@tailwindcss/typography')],
     },
-  },
-  plugins: [],
-}
+};
